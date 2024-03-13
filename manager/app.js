2e53db710e0a9a50ddbf734a51e5498bf31ca078
Aditya Karad <ankarad@uci.edu>
2024-03-13 13:10:49 -0700

    Merged uma branch

--- manager/app.js
+++ manager/app.js
@@ -38,12 +38,22 @@ const app = express();
 // ];
 var amqp = require('amqplib/callback_api');
 const Ingredient = require("./Ingredient");
-
+const STOCK_THRESHOLD = 12;
 connectDB();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+function getSauceStock() {
+    // Assuming a simple variable for sauce stock, replace with your actual logic
+    return 5;
+}
+
+// Mock function to get dough stock
+function getDoughStock() {
+    // Assuming a simple variable for dough stock, replace with your actual logic
+    return 5;
+}
 const produceTasks = async(orders) => {
     amqp.connect('amqp://localhost', function(error0, connection) {
         if (error0) {
@@ -60,6 +70,25 @@ const produceTasks = async(orders) => {
             channel.assertQueue(queue, {
                 durable: true
             });
+
+            const sauceStock = getSauceStock();
+            const doughStock = getDoughStock();
+
+            // if (sauceStock < STOCK_THRESHOLD || doughStock < STOCK_THRESHOLD) {
+            //     // Add extra dough and sauces to work order
+            //     // Added low priority 5 for these tasks
+            //     if (sauceStock < STOCK_THRESHOLD) {
+            //         const extraSauceQtn = STOCK_THRESHOLD - sauceStock;
+            //         orders.push(createWorkOrder("Prepare Sauce", extraSauceQtn, 5, 5));
+            //         console.log(`Added ${extraSauceQtn} extra sauces to the work order.`);
+            //     }
+            //     if (doughStock < STOCK_THRESHOLD) {
+            //         const extraDoughQtn = STOCK_THRESHOLD - doughStock;
+            //         orders.push(createWorkOrder("Prepare Dough", extraDoughQtn, 5, 5));
+            //         console.log(`Added ${extraDoughQtn} extra dough to the work order.`);
+            //     }
+            //
+            // }
             for(const order of orders){
                 channel.sendToQueue(queue, Buffer.from(JSON.stringify(order)), {
                     persistent: true
@@ -123,7 +152,7 @@ app.get("/load", async(req, res) => {
     let orders = await seedMethods.seedDB();
     seedMethods.deleteExistingIngredients();
     let ingredients = await seedMethods.saveIngredients();
-    // await produceTasks(orders);
+    await produceTasks(orders);
     res.send("OK")
 });
 
